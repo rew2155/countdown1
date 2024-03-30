62a89ea2ccc4a13cc44a3c4715b10901c14c1190
Riley Washington <rileywash02@gmail.com>
2024-03-30 15:14:37 -0400

    added js

---
+++ practice.js
@@ -0,0 +1,76 @@
+/* Beginner Challenge */
+
+/* Do all of these using proper ES6 syntax
+Using the following array of objects,
+(1) Write an arrow function that adds a new entry to bookList*/
+    const addEntry = (title, author) => {
+        bookList.push({title, author});
+    }
+// (2) Write an arrow function that removes a specific book from the bookList
+    const remEntry = (title, author) => {
+        for(let i = 0; i < bookList.length; i++){
+            if(bookList[i].title === title && bookList[i].author === author){
+                bookList.splice(i, 1);
+                i--; //need to decrements i after removing na element
+            }
+        }
+    }
+// (3) Write an arrow function that lists out all the books or all the authors in the book list
+    const listAll = (bookList){
+        for(let i = 0; i < bookList.length; i++){
+            console.log("Book title:" + bookList[i].title + "Book Author: " + bookList[i].author);
+        }
+    }
+
+const bookList = [
+    { title: "Don Quixote", author: "Miguel de Cervantes" },
+    { title: "Ulysses", author: "James Joyce" },
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { title: "Moby Dick", author: "Herman Melville" },
+  ];
+  
+  /* Intermediate Challenge */
+  
+  /* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
+    number is positive or negative using a ternary operator (assume the number will never be zero) */
+  //  const list return
+  
+  /* (5) Write a switch statement for a 'day' variable that prints out something based off of what day of
+    the week it is i.e. if it's Monday, print "good luck" or wednesday print "hump day" or friday print "party" */
+  
+  /* (6) Write an arrow function that takes in a number, and uses a for loop to return the sum of every
+    number from 1 up to that number
+    ex. sumUp(7) = 28 */
+  
+  /* Harder Challenge */
+  
+  /* (7) Write an arrow function that converts the temperature from Celsius to Fahrenheit and then tells
+      me what I should wear accordingly */
+  
+  /* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
+    using .forEach() */
+  
+  const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
+  
+  /* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
+    and an averageGrade field representing the letter grade that corresponds to their GPA */
+  
+  const attendance = [
+    { firstName: "Clay", lastName: "Tondreau", gpa: 4.0 },
+    { firstName: "Tucker", lastName: "Wilson", gpa: 2.0 },
+    { firstName: "Eliza", lastName: "Tobin", gpa: 3.7 },
+    { firstName: "Sofia", lastName: "Ackerman", gpa: 1.1 },
+    { firstName: "Thomas", lastName: "Beddow", gpa: 2.3 },
+    { firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
+    { firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
+  ];
+  
+  /* Hardest Challenge (Don't do this without completing harder challenges) */
+  
+  /* Write a function that solves the "every number eventually equals 4" puzzle. The output should be
+      an array of the path you took to make it equal four
+      ex/ [11, 6, 3, 5, 4], [19, 8, 5, 4] or [252, 18, 8, 5, 4]
+      For context: https://puzzling.stackexchange.com/questions/29137/every-number-eventually-equals-4 */
+  
+  // assuming num < 1,000,000. Pattern holds with higher numbers but just requires more checks
+  // does not support leading zeros
